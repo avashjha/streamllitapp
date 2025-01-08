6cf85ccb4c85e4b8319ab5014b9b5a1d7d69b2b5
Avash Jha <avashjhaymail@gmail.com>
2025-01-08 13:36:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -267,11 +267,10 @@ def main():
 
                im1 = Image.open(file_upload).convert("RGB")
                images = [im1]
-               im1.save(f"out.pdf",save_all=True,appen_images=images)
                output_buffer = io.BytesIO()
-               pdf_file.save(output_buffer)
+               im1.save(f"out.pdf",save_all=True).pdf_file.save(output_buffer)
                pdf_bytes = output_buffer.getvalue()
-               st.download_button(label="Download Image",data=pdf_bytes,file_name="Highlighted Script.pdf",mime="application/pdf")
+               st.download_button(label="Download Image",data=pdf_bytes,file_name="Test.pdf",mime="application/pdf")
 
 
 
